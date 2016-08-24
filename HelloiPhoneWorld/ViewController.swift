28ba021c0d5874a28c6b7033f1355290a32e04d2
Alan Sánchez Vázquez <Alan@alans-macbook.lan>
2016-08-24 12:38:59 -0500

    first commit

--- HelloiPhoneWorld/ViewController.swift
+++ HelloiPhoneWorld/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usuarioTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func login(_ sender: AnyObject) {
+        if ((usuarioTextField.text?.compare("snchz")) != nil) {
+            loginLabel.text = "inicio sesion \(passwordTextField.text!)"
+        }
+        view.endEditing(true)
+    }
 }
-
